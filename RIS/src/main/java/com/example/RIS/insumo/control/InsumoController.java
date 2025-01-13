84247aad3ca50aa1b4624b34119a08c2f896c397
Oscarinho10 <20233tn143@utez.edu.mx>
2025-01-13 00:44:26 -0600

    Poquitos cambios

--- RIS/src/main/java/com/example/RIS/insumo/control/InsumoController.java
+++ RIS/src/main/java/com/example/RIS/insumo/control/InsumoController.java
@@ -1,8 +1,10 @@
 package com.example.RIS.insumo.control;
 
 import com.example.RIS.insumo.model.Insumo;
+import com.example.RIS.turno.model.Turno;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/insumos")
+@CrossOrigin(origins = "http://127.0.0.1:5500")  // Allow CORS for this controller
 public class InsumoController {
     @Autowired
     private InsumoService insumoService;
+
+    @GetMapping
+    public List<Insumo> listarTurnos() {
+        return insumoService.listarTodosLosInsumos();
+    }
 
     @GetMapping("/reporte")
     public ResponseEntity<List<Insumo>> generarReporteInsumos() {
